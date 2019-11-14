7fd61c42fc23a8d4957b2e60f52ddcab31a2c440
Chris J. Karr <chris@audacious-software.com>
2019-11-14 15:01:58 +0000

    Initial commit

---
+++ apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class PassiveDataKitExternalDataConfig(AppConfig):
+    name = 'passive_data_kit_external_data'
