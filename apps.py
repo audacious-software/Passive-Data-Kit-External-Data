17bd1f90894fe6adac07efad0785ab243e424552
Chris J. Karr <chris@audacious-software.com>
2021-12-28 18:13:08 +0000

    Specifying default ID type

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
 class PassiveDataKitExternalDataConfig(AppConfig):
     name = 'passive_data_kit_external_data'
     verbose_name = 'Passive Data Kit: External Data'
+    default_auto_field = 'django.db.models.AutoField'
