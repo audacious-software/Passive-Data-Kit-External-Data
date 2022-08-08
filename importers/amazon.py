326a6b6b00eb73fe4bdcfbfc90f7c665700618b9
Chris J. Karr <chris@audacious-software.com>
2022-08-08 15:57:06 +0000

    Amazon importer improvements for business uploads

--- importers/amazon.py
+++ importers/amazon.py
@@ -4,10 +4,13 @@ from __future__ import print_function
 
 from builtins import str # pylint: disable=redefined-builtin
 
+import json
 import math
 
 import arrow
+import chardet
 import pandas
+import pathlib2
 
 from django.conf import settings
 
@@ -29,7 +32,31 @@ DROP_COLUMNS = (
     'Carrier Name & Tracking Number',
     'Buyer Name',
     'Group Name',
+    'Account Group',
+    'PO Number',
+    'Payment Reference ID',
+    'Payment Date',
+    'Payment Amount',
+    'Payment Instrument Type',
+    'Payment Identifier',
+    'Company Compliance',
+    'Receiving Status',
+    'Received Quantity',
+    'Received Date',
+    'Receiver Name',
+    'Receiver Email',
+    'GL Code',
+    'Department',
+    'Cost Center',
+    'Project Code',
+    'Company Compliance',
+    'Approver',
+    'Order Receiving Status',
+    'Order Received Quantity',
+    'Account User',
+    'Account User Email',
 )
+
 
 def process_item(request_identifier, item):
     order_date_str = item.get('Order Date', None)
@@ -55,32 +82,32 @@ def process_item(request_identifier, item):
 
                 pdk_item = {
                     'item': item['Title'],
-                    'category': item['Category'],
-                    'asin': item['ASIN/ISBN'],
-                    'unspc_code': item['UNSPSC Code'],
-                    'website': item['Website'],
-                    'condition': item['Condition'],
-                    'seller': item['Seller'],
+                    'category': item.get('Category', item.get('Segment', None)),
+                    'asin': item.get('ASIN/ISBN', item.get('ASIN', None)),
+                    'unspc_code': item.get('UNSPSC Code', item.get('UNSPSC', None)),
+                    'website': item.get('Website', None),
+                    'condition': item.get('Condition', item.get('Product Condition', None)),
+                    'seller': item.get('Seller', item.get('Seller Name', None)),
                     'seller_credentials': item['Seller Credentials'],
-                    'list_price': item['List Price Per Unit'],
-                    'purchase_price': item['Purchase Price Per Unit'],
+                    'list_price': item.get('List Price Per Unit', item.get('Listed PPU', None)),
+                    'purchase_price': item.get('Purchase Price Per Unit', item.get('Purchase PPU', None)),
                     'purchase_subtotal': item['Item Subtotal'],
-                    'purchase_tax': item['Item Subtotal Tax'],
-                    'purchase_total': item['Item Total'],
+                    'purchase_tax': item.get('Item Subtotal Tax', item.get('Item Tax', None)),
+                    'purchase_total': item.get('Item Total', item.get('Item Net Total', None)),
                     'tax_exemption_applied': item['Tax Exemption Applied'],
                     'tax_exemption_type': item['Tax Exemption Type'],
-                    'exemption_opt_out': item['Exemption Opt-Out'],
+                    'exemption_opt_out': item.get('Exemption Opt-Out', item.get('Tax Exemption Opt Out', None)),
                     'currency': item['Currency'],
-                    'quantity': item['Quantity'],
-                    'zipcode': item.get('Shipping Address Zip', ''),
+                    'quantity': item.get('Quantity', item.get('Item Quantity', None)),
+                    'zipcode': item.get('Shipping Address Zip', item.get('Seller ZipCode', '')),
                     'carrier_name': carrier_name,
-                    'status': item['Order Status'],
+                    'status': item.get('Order Status', ''),
                     'ordered': order_date.date().isoformat(),
                     'pdk_hashed_order_id': hash_content(item['Order ID']),
                 }
 
 
-                ship_date = item['Shipment Date']
+                ship_date = item.get('Shipment Date', '')
 
                 if isinstance(ship_date, str) and ship_date != '':
                     try:
@@ -90,7 +117,7 @@ def process_item(request_identifier, item):
 
                     pdk_item['shipped'] = ship_date.date().isoformat()
 
-                release_date = item['Release Date']
+                release_date = item.get('Release Date', '')
 
                 if isinstance(release_date, str) and release_date != '':
                     release_date = arrow.get(item['Release Date']).replace(tzinfo=settings.TIME_ZONE, hour=12)
@@ -114,7 +141,17 @@ def process_item(request_identifier, item):
 
 
 def import_data(request_identifier, path): # pylint: disable=too-many-branches
-    data_frame = pandas.read_csv(path)
+    detected = chardet.detect(pathlib2.Path(path).read_bytes())
+
+    encoding = detected.get('encoding')
+
+    if detected.get('confidence', 0) < 0.75:
+        print('Detected: %s' % json.dumps(detected, indent=2))
+        print('Falling back to "unicode_escape".')
+
+        encoding = 'unicode_escape'
+
+    data_frame = pandas.read_csv(path, encoding=encoding)
 
     for index, row in data_frame.iterrows(): # pylint: disable=unused-variable
         process_item(request_identifier, row)
